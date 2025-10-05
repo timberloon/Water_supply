7c8578bbd337d4605c1c0083f2f20f5c79d2336d
Timberloon <bigchungus02004@gmail.com>
2025-10-05 22:41:12 +0530

    added dlls

--- render.cpp
+++ render.cpp
@@ -3,5 +3,14 @@
 #include"graph.hpp"
 
 int main(){
-    std::cout<< "Hello world";
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    SDL_CreateWindowAndRenderer("First window",800,600,SDL_WINDOW_RESIZABLE,&window,&renderer);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
 }
